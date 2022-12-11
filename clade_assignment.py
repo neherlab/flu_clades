0f31016d7f08f4f4a4a8bd00b2627442a8f011b5
Richard Neher <richard.neher@unibas.ch>
2022-12-11 21:22:52 +0100

    reworked assignment algorithm

--- clade_assignment.py
+++ clade_assignment.py
@@ -2,7 +2,7 @@ import json, argparse
 import urllib.request, json
 from collections import defaultdict
 import numpy as np
-
+from config.weights import weights
 
 def assign_alive(n, max_date, min_date):
     '''
@@ -15,7 +15,7 @@ def assign_alive(n, max_date, min_date):
         numdate = n['node_attrs']['num_date']['value']
         n['alive'] = 1 if numdate<max_date and numdate>min_date else 0
 
-def gather_tip_counts(n, attenuate=None):
+def gather_tip_counts(n, attenuate=None, max_value=0, ignore_clade_breakpoints=False):
     '''
     Assigns a node attribute "ntips" to each node that counts the number of downstream tips.
     This can be attenuated similar to the LBI calculation.
@@ -23,11 +23,18 @@ def gather_tip_counts(n, attenuate=None):
     if "children" in n:
         tmp = 0
         for c in n["children"]:
-            gather_tip_counts(c, attenuate=attenuate)
-            tmp += c["ntips"]*attenuate(c)
+            tmp_max = gather_tip_counts(c, attenuate=attenuate, max_value=max_value, ignore_clade_breakpoints=ignore_clade_breakpoints)
+            if (ignore_clade_breakpoints and 'clade' in c['branch_attrs'].get('labels', {}))==False:
+                tmp += c["ntips"]*attenuate(c)
+            if tmp_max>max_value:
+                max_value = tmp_max
         n["ntips"] = tmp
     else:
         n["ntips"] = 1 if n['alive'] else 0
+
+    max_value = n["ntips"] if n["ntips"]>max_value else max_value
+
+    return max_value
 
 def assign_divergence(n, gene='HA1'):
     '''
@@ -35,15 +42,8 @@ def assign_divergence(n, gene='HA1'):
     '''
     if "children" in n:
         for c in n["children"]:
-            c['div'] = n['div'] + len(c['branch_attrs']['mutations'].get('HA1',[]))
+            c['div'] = n['div'] + len(c['branch_attrs']['mutations'].get(gene,[]))
             assign_divergence(c)
-
-def assign_score(n, score=None):
-    if "children" in n:
-        for c in n["children"]:
-            assign_score(c, score=score)
-    n['node_attrs']["score"] = {'value': score(n)}
-
 
 def maxval(n, attr, val=-np.inf, best_node=None):
     if best_node is None:
@@ -70,12 +70,6 @@ def first_above(n, attr, cutoff):
                     return choice
         return None
 
-def assign_clade(n, clade):
-    if "children" in n:
-        for c in n["children"]:
-            assign_clade(c, clade)
-    n['node_attrs']["trial_clade"] = {'value': clade}
-
 def remove_attr(n, attr):
     if "children" in n:
         for c in n["children"]:
@@ -97,43 +91,56 @@ def rename_clade_labels(n, name_map):
         for c in n['children']:
             rename_clade_labels(c, name_map)
 
-def score(n):
-    if n['div']<2:
-        return 0.0
-
-    score = n['ntips']
-    nmuts = len(n['branch_attrs']['mutations'].get('HA1',[]))
-    score += 20*nmuts/(4 + nmuts)
-    # if 'labels' in n['branch_attrs']:
-    #     if "clade" in n['branch_attrs']['labels']:
-    #         score += 100
+def score(n, weights=None, ntip_scale=1):
+    if weights is None:
+        weights = {}
+    score = np.sqrt(n['ntips']/ntip_scale)
+    mut_weight = sum([weights.get(int(x[1:-1]),1) for x in n['branch_attrs']['mutations'].get('HA1',[])])
+    if mut_weight>0:
+        score += mut_weight/(4 + mut_weight)
+    else:
+        score = 0.0
     return score
 
-def assign_clades_to_branch(n, hierarchy):
-    node = first_above(n, 'score', 20)
-    if node is not None:
-        print("new clade",node['name'], node['div'], node['ntips'], node['node_attrs']['score']['value'])
-        node['div']=0
-        if 'labels' not in node['branch_attrs']:
-            node['branch_attrs']['labels'] = {}
-        assign_divergence(node)
-        node["node_attrs"]["clade_score"] = {"value":node["node_attrs"]["score"]["value"]}
-        print(node["node_attrs"]["clade_score"])
-        assign_score(node, score)
-        parent_clade = node['node_attrs']["trial_clade"]["value"]
-        new_suffix = len(hierarchy[parent_clade])+1
-        new_clade = tuple(list(parent_clade) + [new_suffix])
-        hierarchy[parent_clade].append(new_suffix)
-        node['branch_attrs']['labels']['trial_clade'] = new_clade
-        assign_clade(node, new_clade)
-        hierarchy[new_clade] = []
-        clades[new_clade] = node
 
-    if 'children' in n:
+def assign_score(n, score=None, weights=None, ntip_scale=1):
+    if "children" in n:
         for c in n["children"]:
-            hierarchy = assign_clades_to_branch(c, hierarchy)
+            assign_score(c, score=score, weights=weights, ntip_scale=ntip_scale)
+    n['node_attrs']["score"] = {'value': score(n, weights=weights, ntip_scale=ntip_scale)}
 
-        print(node["node_attrs"]["clade_score"])
+
+def assign_clade(n, clade):
+    if "children" in n:
+        for c in n["children"]:
+            assign_clade(c, clade)
+    n['node_attrs']["trial_clade"] = {'value': clade}
+
+def assign_clades_to_branch(n, hierarchy, cutoff=1.0):
+    node = first_above(n, 'score', cutoff)
+    if node is not None:
+        if "clade_score" not in node["node_attrs"]:
+            print("new clade",node['name'], node['div'], node['ntips'], node['node_attrs']['score']['value'])
+
+            if 'labels' not in node['branch_attrs']:
+                node['branch_attrs']['labels'] = {}
+
+            node["node_attrs"]["clade_score"] = {"value":node["node_attrs"]["score"]["value"]}
+            print(node["node_attrs"]["clade_score"])
+
+            parent_clade = node['node_attrs']["trial_clade"]["value"]
+            new_suffix = len(hierarchy[parent_clade])+1
+            new_clade = tuple(list(parent_clade) + [new_suffix])
+            hierarchy[parent_clade].append(new_suffix)
+            node['branch_attrs']['labels']['trial_clade'] = new_clade
+            assign_clade(node, new_clade)
+
+            hierarchy[new_clade] = []
+            clades[new_clade] = node
+
+        if 'children' in n:
+            for c in n["children"]:
+                hierarchy = assign_clades_to_branch(c, hierarchy, cutoff=cutoff)
 
     return hierarchy
 
@@ -154,16 +161,19 @@ if __name__=="__main__":
 
     clades = {}
     assign_alive(T,max_date,min_date)
-    gather_tip_counts(T, lambda x:np.exp(-len(x['branch_attrs']['mutations'].get('HA1',[]))/1))
+    max_value = gather_tip_counts(T, lambda x:np.exp(-len(x['branch_attrs']['mutations'].get('HA1',[]))/1), ignore_clade_breakpoints=True)
 
     root_clade = ('A',)
-    T["node_attrs"]["clade_score"] = {"value": 100}
     T['div']=0
     assign_divergence(T)
-    assign_score(T, score)
+    assign_score(T, score, weights=weights[args.lineage], ntip_scale=max_value)
+
+
+
+
     assign_clade(T, root_clade)
     hierarchy = {root_clade:[]}
-    hierarchy = assign_clades_to_branch(T, hierarchy)
+    hierarchy = assign_clades_to_branch(T, hierarchy, cutoff=0.75)
 
     aliases = {root_clade: "R"}
     major_clades = defaultdict(list)
@@ -171,7 +181,7 @@ if __name__=="__main__":
     for clade in clades:
         node  = clades[clade]
         print(node["node_attrs"]["clade_score"]["value"])
-        if node["node_attrs"]["clade_score"]["value"]>60:
+        if node["node_attrs"]["clade_score"]["value"]>1:
             year = int(node["node_attrs"]["num_date"]["value"]) - 2000
             letter = alpha[len(major_clades[year])%len(alpha)]
             aliases[node["node_attrs"]["trial_clade"]["value"]] = f"{year}{letter}"
@@ -198,5 +208,5 @@ if __name__=="__main__":
 
     data['meta']['colorings'].append({'key':"trial_clade", 'type':'ordinal', 'title':"trial clade"})
     data['meta']['colorings'].append({'key':"score", 'type':'continuous', 'title':"clade score"})
-    with open(f'trial_{args.lineage}_clades.json', 'w') as fh:
+    with open(f'auspice/trial_{args.lineage}_clades.json', 'w') as fh:
         json.dump(data, fh)
